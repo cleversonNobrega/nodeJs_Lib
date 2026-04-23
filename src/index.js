adda92b91ea11dbefee5dd411a5aa06f6871502c
CLEVERSON  SANTOS <felipecleverson1@gmail.com>
2026-04-23 09:32:53 -0400

    NodeJs_Lib

--- src/index.js
+++ src/index.js
@@ -1,28 +1,44 @@
 const fs = require("fs");
-const { text } = require("stream/consumers");
+import trataErros from "./erro/funcoesErro";
 
 const caminhoArquino = process.argv;
 const link = caminhoArquino[2];
 
-fs.readFile(link, "utf-8", (erro, texto) => {
-  quebraEmParagrafos(texto);
+fs.readFile(link, "uft-8", (erro, texto) => {
+  try {
+    if (erro) throw erro;
+    contaPalavras(texto);
+  } catch (erro) {
+    trataErros(erro);
+  }
 });
 
-function quebraEmParagrafos(texto) {
-  const paragrafos = texto.toLowerCase().split("\n");
-  const contagem = paragrafos.map((paragrafos) => {
-    return verificaPalavraDuplicadas(paragrafos);
+function contaPalavras(texto) {
+  const paragrafos = extraiParagrafos(texto);
+  const contagem = paragrafos.flatMap((paragrafo) => {
+    if (!paragrafo) return [];
+    return verificaPalavraDuplicadas(paragrafo);
   });
+
   console.log(contagem);
 }
 
-function verificaPalavraDuplicadas(texto) {
+function extraiPalavras(texto) {
+  return texto.toLowerCase().split("\n");
+}
+
+function limpaPalavras(palavra) {
+  return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+}
+
+function verificaPalavrasDuplicadas(texto) {
   const listaPalavras = texto.split(" ");
   const resultado = {};
-
-  listaPalavras.forEach((palavras) => {
-    resultado[palavras] = (resultado[palavras] || 0) + 1;
+  listaPalavras.forEach((palavra) => {
+    if (palavra.length >= 3) {
+      const palavraLimpa = limpaPalavras(palavra);
+      resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1;
+    }
   });
-
   return resultado;
 }
