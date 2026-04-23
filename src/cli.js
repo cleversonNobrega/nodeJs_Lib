967454d55dafc3b20772f986a509bc905ff239bb
Felipe Cleverson <felipecleverson1@gmail.com>
2026-04-23 17:30:57 -0400

    NodeJs_Lib

--- src/cli.js
+++ src/cli.js
@@ -3,16 +3,36 @@ import fs from "fs";
 import trataErros from "./erro/funcoesErro.js";
 import { contaPalavras } from "./index.js";
 import { montaSaidaArquivo } from "./helpers.js";
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+  .version("0.0.1")
+  .option("-t, --texto <string>", "caminho do texto a ser processado")
+  .option(
+    "-d, --destino <string>",
+    "caminho da pasta onde salvar o arquivo de resultados",
+  )
+  .action((options) => {
+    const { texto, destino } = options;
+    if (!texto || !destino) {
+      console.error("erro: favor inserir caminho de origem e destino");
+      program.help();
+      return;
+    }
+  });
 
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 const endereco = caminhoArquivo[3];
 
-fs.readFile(link, "utf-8", (erro, texto) => {
+function processaArquivos(texto, destino) {}
+fs.readFile(texto, "utf-8", (erro, texto) => {
   try {
     if (erro) throw erro;
     const resultado = contaPalavras(texto);
-    criaESalvaArquivo(resultado, endereco);
+    criaESalvaArquivo(resultado, destino);
   } catch (erro) {
     trataErros(erro);
   }
