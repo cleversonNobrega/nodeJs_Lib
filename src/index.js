8c4eb7e0ef2e29d830c79b18c94d26f0808a0bf6
CLEVERSON  SANTOS <felipecleverson1@gmail.com>
2026-04-22 20:44:39 -0400

    NodeJs_Lib

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 const fs = require("fs");
+const { text } = require("stream/consumers");
 
 const caminhoArquino = process.argv;
 const link = caminhoArquino[2];
 
 fs.readFile(link, "utf-8", (erro, texto) => {
-  console.log(texto);
+  quebraEmParagrafos(texto);
 });
+
+function quebraEmParagrafos(texto) {
+  const paragrafos = texto.toLowerCase().split("\n");
+  const contagem = paragrafos.map((paragrafos) => {
+    return verificaPalavraDuplicadas(paragrafos);
+  });
+  console.log(contagem);
+}
+
+function verificaPalavraDuplicadas(texto) {
+  const listaPalavras = texto.split(" ");
+  const resultado = {};
+
+  listaPalavras.forEach((palavras) => {
+    resultado[palavras] = (resultado[palavras] || 0) + 1;
+  });
+
+  return resultado;
+}
