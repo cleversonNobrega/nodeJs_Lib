9edcc021df8bbfe7fd441a095dc7802f7810b7e9
Felipe Cleverson <felipecleverson1@gmail.com>
2026-04-23 22:28:13 -0400

    NodeJs_Lib

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,10 @@
 import fs from "fs";
-
-import trataErros from "./erro/funcoesErro.js";
+import path from "path";
+import trataErros from "./erros/funcoesErro.js";
 import { contaPalavras } from "./index.js";
 import { montaSaidaArquivo } from "./helpers.js";
 import { Command } from "commander";
+import chalk from "chalk";
 
 const program = new Command();
 
@@ -16,39 +17,47 @@ program
   )
   .action((options) => {
     const { texto, destino } = options;
+
     if (!texto || !destino) {
-      console.error("erro: favor inserir caminho de origem e destino");
+      console.error(
+        chalk.red("erro: favor inserir caminho de origem e destino"),
+      );
       program.help();
       return;
     }
+
+    const caminhoTexto = path.resolve(texto);
+    const caminhoDestino = path.resolve(destino);
+
+    try {
+      processaArquivo(caminhoTexto, caminhoDestino);
+      console.log(chalk.green("texto processado com sucesso"));
+    } catch (erro) {
+      console.log("ocorreu um erro no processamento", erro);
+    }
   });
 
-const caminhoArquivo = process.argv;
-const link = caminhoArquivo[2];
-const endereco = caminhoArquivo[3];
+program.parse();
 
-function processaArquivos(texto, destino) {}
-fs.readFile(texto, "utf-8", (erro, texto) => {
-  try {
-    if (erro) throw erro;
-    const resultado = contaPalavras(texto);
-    criaESalvaArquivo(resultado, destino);
-  } catch (erro) {
-    trataErros(erro);
-  }
-});
+function processaArquivo(texto, destino) {
+  fs.readFile(texto, "utf-8", (erro, texto) => {
+    try {
+      if (erro) throw erro;
+      const resultado = contaPalavras(texto);
+      criaESalvaArquivo(resultado, destino);
+    } catch (erro) {
+      trataErros(erro);
+    }
+  });
+}
 
-function criaESalvaArquivo(listaPalavras, endereco) {
+async function criaESalvaArquivo(listaPalavras, endereco) {
   const arquivoNovo = `${endereco}/resultado.txt`;
   const textoPalavras = montaSaidaArquivo(listaPalavras);
-
-  fs.promises
-    .writeFile(arquivoNovo, textoPalavras)
-    .then(() => {
-      console.log("arquivo criado");
-    })
-    .catch((erro) => {
-      throw erro;
-    })
-    .finally(() => console.log("operação finalizada"));
+  try {
+    await fs.promises.writeFile(arquivoNovo, textoPalavras);
+    console.log("arquivo criado");
+  } catch (erro) {
+    throw erro;
+  }
 }
